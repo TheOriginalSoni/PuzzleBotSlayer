a356a2d366e555f904341ee2603be7cc9a8a4476
Dan Simon <des480@nyu.edu>
2024-01-25 15:48:19 -0500

    Add solvertools

---
+++ solvertools/ciphers.py
@@ -0,0 +1,106 @@
+import string
+from solvertools.wordlist import WORDS
+from itertools import cycle
+ASCII_a = 97
+
+
+def shift_letter(char, offset):
+    if char not in string.ascii_letters:
+        return char
+    idx = ord(char.lower()) - ASCII_a
+    new_idx = (idx + offset) % 26
+    new_char = chr(ASCII_a + new_idx)
+    if char.isupper():
+        return new_char.upper()
+    else:
+        return new_char
+
+
+def caesar_shift(text, offset):
+    """
+    Performs a Caesar shift by the given offset.
+
+    If the offset is a letter, it will look it up in the alphabet to convert
+    it to a shift. (For example, a shift of 'C' means that 'A' goes to 'C',
+    which is the same as a shift of 2.)
+        
+        >>> print(caesar_shift('caesar', 13))
+        pnrfne
+        >>> print(caesar_shift('CAESAR', 'C'))
+        ECGUCT
+
+    """
+    if isinstance(offset, str):
+        offset = ord(offset.lower()) - ASCII_a
+
+    shifted = [shift_letter(ch, offset) for ch in text]
+    return ''.join(shifted)
+
+
+def caesar_unshift(text, offset):
+    """
+    Performs a Caesar shift backwards by the given offset.
+    
+    If the offset is a letter, it will look it up in the alphabet to convert
+    it to a shift. (For example, a shift of 'C' means that 'C' goes to 'A',
+    which is the same as a backward shift of 2.)
+
+        >>> print(caesar_unshift('DBFTBS TIJGU', 1))
+        CAESAR SHIFT
+    """
+    if isinstance(offset, str):
+        offset = ord(offset.lower()) - ASCII_a
+    return caesar_shift(text, -offset)
+
+
+def best_caesar_shift(text, wordlist=WORDS, count=5):
+    """
+    Find the most cromulent Caesar shift of a ciphertext.
+    """
+    possibilities = [caesar_shift(text, n) for n in range(26)]
+    results = []
+    for n, poss in enumerate(possibilities):
+        results.extend([found + (n,) for found in wordlist.search(poss)])
+    return wordlist.show_best_results(results, count=count)
+
+
+def vigenere_encode(text, key, one_based=False):
+    """
+    Apply the Vigenere cipher to `text`, with `key` as the key.
+
+    In this cipher, A + A = A, but in many cases in the Mystery Hunt,
+    A + A = B. To get the A + A = B behavior, set `one_based` to true.
+
+    >>> vigenere_encode('ABRACADABRA', 'abc')
+    'ACTADCDBDRB'
+    >>> vigenere_encode('ABRACADABRA', 'abc', one_based=True)
+    'BDUBEDECESC'
+    """
+    shifted = []
+    letters = [ch for ch in text if ch in string.ascii_letters]
+    shifted = [caesar_shift(ch, shift)
+               for (ch, shift) in zip(letters, cycle(key))]
+    result = ''.join(shifted)
+    if one_based:
+        result = caesar_shift(result, 1)
+    return result
+
+
+def vigenere_decode(text, key, one_based=False):
+    """
+    Decode a Vigenere cipher on `text`, with `key` as the key.
+
+    In this cipher, B - B = A, but in many cases in the Mystery Hunt,
+    B - B = Z. To get the B - B = Z behavior, set `one_based` to true.
+
+    >>> vigenere_decode(vigenere_encode('ABRACADABRA', 'abc'), 'abc')
+    'ABRACADABRA'
+    """
+    shifted = []
+    letters = [ch for ch in text if ch in string.ascii_letters]
+    shifted = [caesar_unshift(ch, shift)
+               for (ch, shift) in zip(letters, cycle(key))]
+    result = ''.join(shifted)
+    if one_based:
+        result = caesar_shift(result, -1)
+    return result
---
+++ solvertools/search.py
@@ -0,0 +1,136 @@
+from solvertools.wordlist import WORDS
+from solvertools.normalize import slugify, sanitize
+from solvertools.util import data_path, db_path
+from operator import itemgetter
+from collections import defaultdict
+from unidecode import unidecode
+from .conceptnet_numberbatch import load_numberbatch, get_vector, similar_to_term
+import re
+import sqlite3
+
+NUMBERBATCH = None
+DB = None
+
+def tokenize(text):
+    return re.findall("[A-Za-z']+", text)
+
+def query_expand(word):
+    global NUMBERBATCH
+    if NUMBERBATCH is None:
+        NUMBERBATCH = load_numberbatch()
+
+    similar = similar_to_term(NUMBERBATCH, word, limit=25)
+    sim_words = [word2.replace('"','') for word2, sim in similar.items() if sim >= 0.2]
+    parts = [word] + [word2 for word2 in sim_words if word2 != word]
+    query = ' OR '.join('"%s"' % word2 for word2 in parts)
+    return '(%s)' % query
+
+
+def db_search(query, limit=10000):
+    global DB
+    if DB is None:
+        DB = sqlite3.connect(db_path("search.db"), check_same_thread=False)
+
+    cur = DB.cursor()
+    results = defaultdict(float)
+    for (keyword, negscore) in cur.execute("SELECT keyword, bm25(clues) AS score FROM clues WHERE text MATCH ? LIMIT ?", (query, limit)):
+        assert negscore < 0
+        results[keyword] -= negscore
+    return results
+
+
+def db_rank(clue):
+    scores = defaultdict(float)
+    for match, score in db_search(clue).items():
+        scores[slugify(match)] += score * 1000
+        parts = tokenize(match)
+        for part in parts:
+            scores[slugify(part)] += score * 1000 / len(parts)
+
+    for word in tokenize(clue):
+        logprob_result = WORDS.segment_logprob(slugify(word))
+        if logprob_result is not None:
+            logprob, _ = logprob_result
+        else:
+            logprob = -1000.
+        rare_boost = min(25., -logprob)
+        for match, score in db_search(word).items():
+            scores[slugify(match)] += rare_boost * score * 10
+            parts = tokenize(match)
+            for part in parts:
+                scores[slugify(part)] += rare_boost * score * 10 / len(parts)
+
+        query = query_expand(word)
+        for match, score in db_search(query).items():
+            scores[slugify(match)] += rare_boost * score
+            parts = tokenize(match)
+            for part in parts:
+                scores[slugify(part)] += rare_boost * score / len(parts)
+
+    return scores
+
+
+def required_spaces_match(pattern, text):
+    if ' ' not in pattern:
+        return True
+    pattern_re_text = pattern.lstrip('^').rstrip('$')
+    pattern_re = re.compile('^' + pattern_re_text + '$')
+    return bool(pattern_re.match(text.lower()))
+
+
+def search(pattern=None, clue=None, length=None, count=20):
+    """
+    Find words and phrases that match various criteria: a regex pattern,
+    a clue phrase, and/or a length.
+
+    >>> search('.a.b.c..')[0][1]
+    'BARBECUE'
+    >>> search('.a.f....', clue='US President')[0][1]
+    'GARFIELD'
+    >>> search(clue='lincoln assassin', length=15)[0][1]
+    'JOHN WILKES BOOTH'
+
+    If the pattern contains spaces, we require the spacing of the text to match.
+    >>> search('....e .......', clue='NASA vehicle')[0][1]
+    'SPACE SHUTTLE'
+    >>> search('....e.......', clue='NASA vehicle')[0][1]
+    'CARTERCOPTER'
+    >>> search('[jkl][def][def][tuv] [mno][tuv][tuv]')[0][1]
+    'LEFT OUT'
+    """
+    if clue is None:
+        if pattern is None:
+            return []
+        else:
+            scount = count
+            if ' ' in pattern:
+                scount *= 10
+            found = WORDS.search(pattern, count=scount, length=length, use_cromulence=True)
+            results = []
+            for (score, text) in found:
+                if required_spaces_match(pattern, text):
+                    results.append((score, text))
+                    if len(results) >= count:
+                        break
+            if results:
+                return results
+            else:
+                return found[:count]
+
+    if pattern is not None:
+        pattern_re_text = pattern.lstrip('^').rstrip('$').replace(' ', '').lower()
+        pattern_re = re.compile('^' + pattern_re_text + '$')
+    else:
+        pattern_re = None
+
+    raw_matches = sorted(db_rank(clue).items(), key=itemgetter(1), reverse=True)
+    matches = {}
+    for slug, score in raw_matches:
+        if length is None or length == len(slug):
+            if pattern is None or pattern_re.match(slug):
+                crom, text = WORDS.cromulence(slug)
+                if pattern is None or required_spaces_match(pattern, text):
+                    matches[text] = score
+        if len(matches) >= count:
+            break
+    return sorted([(score, text) for (text, score) in matches.items()], reverse=True)
